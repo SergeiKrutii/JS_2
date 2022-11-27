b5a4d62aaa10b5ba7a2ad296224ce7bd39b1d6ca
SergeiKrutii <106181150+SergeiKrutii@users.noreply.github.com>
2022-11-27 23:34:53 +0200

    and task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imgContainerRef = document.querySelector('.gallery');
+const imagesMarkup = makeMarkup(galleryItems);
 
-console.log(galleryItems);
+imgContainerRef.insertAdjacentHTML('beforeend', imagesMarkup);
+imgContainerRef.addEventListener('click', onShowModal);
+
+
+function makeMarkup(obj) {
+    return obj.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>  
+        `
+    }).join('');
+}
+
+
+function onShowModal(e) {
+    e.preventDefault();
+    const dataValue = e.target.dataset.source;
+
+    if (e.target.tagName === 'IMG') {
+        const instance = basicLightbox.create(`
+        <img src="${dataValue}" width="800" height="600">
+    `)
+    
+        instance.show()
+        
+        if (basicLightbox.visible()) {
+            imgContainerRef.addEventListener('keydown', (e) => {
+                if (e.code === 'Escape') {
+                    instance.close();
+                    }
+                })
+            }   
+    }
+};
+
+
+
+    
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imgContainerRef = document.querySelector('.gallery');
+const imagesMarkup = makeMarkup(galleryItems);
 
-console.log(galleryItems);
+
+imgContainerRef.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function makeMarkup(obj) {
+    
+    return obj.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" />
+        </a>
+        `
+    }).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+});
+
+
